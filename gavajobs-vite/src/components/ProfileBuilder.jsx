760729b497ccb680998e89e18d99c0167bc3f90f
allannjorogem <allannjorogem@gmail.com>
2026-05-26 17:28:17 +0300

    Update ProfileBuilder.jsx

--- gavajobs-vite/src/components/ProfileBuilder.jsx
+++ gavajobs-vite/src/components/ProfileBuilder.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import React, { useState, useMemo } from 'react'
 import { C } from '../constants/theme'
 import { dl, matchColor } from '../utils/helpers'
 import { isManagement } from '../services/matchingEngine'
